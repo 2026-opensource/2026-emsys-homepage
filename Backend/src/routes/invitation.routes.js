883984b9849cdccd502463c5485789f8e623f112
Abyss <milo04@naver.com>
2026-07-13 22:05:38 +0900

    cache-control 추가

--- Backend/src/routes/invitation.routes.js
+++ Backend/src/routes/invitation.routes.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 const invitationController = require("../controllers/invitation.controller");
 
-router.get("/", invitationController.getInvitationCode);
+router.get(
+    "/",
+    (req, res, next) => {
+        res.set("Cache-Control", "no-store");
+        next();
+    },
+    invitationController.getInvitationCode
+);
 
 module.exports = router;
